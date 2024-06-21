30439c3e9dabdd311193115dde98fbe0f6e96b32
Ahmed Khaled <you@example.com>
2024-06-21 06:49:37 +0300

    SUBNAV

--- src/Layouts/SubNavBar.js
+++ src/Layouts/SubNavBar.js
@@ -2,28 +2,25 @@ import React from 'react'
 
 function SubNavBar() {
   return (
-  <div className="top">
-<div className="container">
-    <div className='row'>
-      <div className="col-md-8 d-flex align-items-center">
-        <div className="text-left">
-          <span><i className="fas fa-location-dot"></i> Brooklyn, NY 10036, United States </span>
+    <div>
+    <div className="top">
+
+     <div class="zerogrid">
+        
+          <div className=" f-left">
+            <span><i className="fa fa-map-marker"></i> Brooklyn, NY 10036, United States </span>
+            <span><i className="fa fa-phone"></i> 1-800-123-1234; 1-800-123-5678</span>
+            <span><i className="fa fa-envelope"></i> example.com</span>
+          </div>
+          <div className="col-md-6  f-right">
+            <span>Don't walk through life just playing football.</span>
+          </div>
         </div>
-        <div className="text-left">
-          <span><i className="fa fa-phone"></i> 1-800-123-1234; 1-800-123-5678</span>
-        </div>
-        <div className="text-left">
-          <span><i className="fa fa-envelope"></i> example.com</span>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="text-left">
-          <span>Don't walk through life just playing football.</span>
-        </div>
-      </div>
       </div>
     </div>
-  </div>
+  
+  
+
 
   
 
